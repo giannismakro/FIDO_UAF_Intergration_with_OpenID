62757144822fd80f1a436bbaea058af29ffd3ecc
giannismakro <75633664+giannismakro@users.noreply.github.com>
2021-09-21 12:37:18 +0300

    Update fidouafauth.js

--- Keycloak_script_authenticator-master/Keycloak_script_authenticator-master/keycloakscriptauthenticator/fidouafauth.js
+++ Keycloak_script_authenticator-master/Keycloak_script_authenticator-master/keycloakscriptauthenticator/fidouafauth.js
@@ -9,24 +9,19 @@ const FidoAuthenticator = createAuthenticator(
     processNew: async function (req) {
       console.info(`\n[${new Date()}] New request from Keycloak received for User: ${req.user.username}`);
 	 console.info(`\n[${req.user.attributes[this.fidoAuthenticationIdAttribute]}`);
-      /*
-        Send an HTTP GET Request to the FIDO2 Server to get the last fidoAuthenticationId 
-        of the user and compare it with the fidoAuthenticationId attribute value of the user
-       */
-      // await axios.get(`http://localhost:8090/fido2/lastAuthId/${req.user.username}`).then((resp) => {
+     
 	  await axios.get(`http://localhost:8080/fido/v1/isauth/${req.user.attributes[this.fidoAuthenticationIdAttribute]}`).then((resp) => {
-     // await axios.get("https://api.github.com/users/janbodnar").then((resp) => {
+    
 	var response=JSON.stringify(resp.data);
 	var obj=JSON.parse(response)
 	 console.log(obj.authenticated);
-        // this.latestFidoAuthenticationId = resp.data.lastAuthId;
+       
 		this.latestFidoAuthenticationId = obj.authenticated;
 		this.usersuname=obj.authenticated=obj.username;
-        //this.latestFidoAuthenticationId = "test1234";
+       
       });
 
-      // Check if the FIDO AuthenticationId attribute of the user matches the last
-      // FIDO AuthenticationId returned from the FIDO2 Server for that user
+    
       if (
         //req.user &&
         //req.user.attributes[this.fidoAuthenticationIdAttribute] ==
@@ -37,7 +32,7 @@ const FidoAuthenticator = createAuthenticator(
       } else {
         // const createUserAttributes = {};
         // createUserAttributes[this.fidoAuthenticationIdAttribute] =
-        //   "Invalid FIDO2 AuthenticationId";
+   
         return {
           // username: req.user.username,
           // userAttributes: createUserAttributes,
